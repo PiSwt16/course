58b4dd48e15290afbfa68149b9644f463d110441
PiSwt16 <piswt16@outlook.com>
2019-02-05 17:08:07 +0700

    up

--- hackerrank/array_reverse.cpp
+++ hackerrank/array_reverse.cpp
@@ -11,12 +11,7 @@ void print(const int& n) {
     std::cout << n << ' '; 
 };
  
-
 int main() {
-    int a(10), b(20);
-    cout << a - b << endl;
-    cout << abs(a - b) << endl;
-    
     int N(5);
     vector<int> v;
     v.resize(N);
@@ -26,16 +21,24 @@ int main() {
     for(int i = 0; i < N; i++) {
         cout << v[i] << ' ';
     }
-    // for (const auto& aa : v) {  
-    //     cout << aa << endl;
-    // }
+    cout << endl;
+    
+    for (const auto& aa : v) {  
+        cout << aa << endl;
+    }
     
     cout << endl;
     
     std::for_each(v.begin(), v.end(), print);
     cout << endl;
-    // std::for_each(v.begin(), v.end(), [](int &n){ n++; });
-    // cout << endl;
+
+    std::for_each(v.begin(), v.end(), [](int &n){ n++; });
+    cout << endl;
+
+    for (vector<int>::iterator it=v.begin(); it!=v.end(); ++it) 
+        cout << ' ' << *it;
+    cout << '\n';
+
     std::reverse(v.begin(),v.end()); 
     std::for_each(v.begin(), v.end(), print);
     cout << endl;
